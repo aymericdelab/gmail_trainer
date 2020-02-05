d8dd95927aa0f36013efdb8beaf24528ad46904a
aymericdelab <38816784+aymericdelab@users.noreply.github.com>
2020-02-05 18:43:07 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from distutils.core import setup
 setup(
   name = 'gmail_trainer',         # How you named your package folder (MyLib)
   packages = ['gmail_trainer'],   # Chose the same as "name"
-  version = '0.5',      # Start with a small number and increase it with every change you make
+  version = '0.6',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'get mail notifications when your training is completed',   # Give a short description about your library
   author = 'Aymeric de la Brousse',                   # Type in your name
   author_email = 'aymericdelabrousse@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/aymericdelab/gmail_trainer',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/aymericdelab/gmail_trainer/archive/v_05.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/aymericdelab/gmail_trainer/archive/v_06.tar.gz',    # I explain this later on
   keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
           'google-api-python-client',
